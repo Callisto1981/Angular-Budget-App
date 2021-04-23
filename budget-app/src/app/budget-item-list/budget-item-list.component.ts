72aff9c91adfe500826c05568b45fbb8326765f6
Wayne <wabbruscatoorr@gmail.com>
2021-04-23 14:18:44 -0400

    Updating budget item list

--- budget-app/src/app/budget-item-list/budget-item-list.component.ts
+++ budget-app/src/app/budget-item-list/budget-item-list.component.ts
@@ -1,5 +1,7 @@
  import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
  import { BudgetItem } from 'src/shared/models/budget-item.model';
+ import { MatDialog } from '@angular/material';
+import { EditItemModalComponent } from '../edit-item-modal/edit-item-modal.component';
 
 @Component({
     selector: 'app-budget-item-card',
@@ -11,12 +13,24 @@
      @Input() budgetItems: BudgetItem[];
      @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
 
-     constructor() { }
+     constructor(public dialog: MatDialog) { }
 
      ngOnInit() {
      }
 
      ondeleteButtonClicked(item: BudgetItem) {
         this.delete.emit(item);
+     }
+
+     onCardClicked(item: BudgetItem) {
+         const dialogRef = this.dialog.open(EditItemModalComponent, {
+            width: '500px',
+            data: item
+         });
+         dialogRef.afterClosed().subscribe(result => {
+             if (result) {
+                 this.budgetItems[this.budgetItems.indexOf(item)] = result;
+             }
+         })
      }
  }
